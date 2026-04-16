0ab0cbc57874cc3f9bb3f665b72ce7b3febf4ddd
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2026-04-16 06:45:18 -0700

    documentation tweaks

--- R/AirSensor2-package.R
+++ R/AirSensor2-package.R
@@ -2,13 +2,11 @@
 "_PACKAGE"
 
 #' @name AirSensor2
-#' @title Air Quality Data Analysis for low-cost Sensors
+#' @title Air Quality Data Analysis for Low-Cost Sensors
 #' @description
-#' \code{
 #' Utilities for working with air quality monitoring data from low-cost sensors
 #' with a focus on small particulates (PM2.5). Initial focus is on sensors
 #' produced by 'PurpleAir' <https://www2.purpleair.com>.
-#' }
 #'
 #' @references [PurpleAir](https://www2.purpleair.com)
 #' @references [PurpleAir API](https://api.purpleair.com)
@@ -23,12 +21,11 @@ NULL
 #' @title US EPA AQI Index levels, names, colors and action text
 #'
 #' @description
-#' Official, US EPA AQI levels, names, colors and action text are provided in a
+#' Official US EPA AQI levels, names, colors, and action text are provided in a
 #' list for easy coloring and labeling.
 #'
 #' See [AirMonitor::US_AQI()] for details.
 #'
-#' @name US_AQI
 #' @rdname US_AQI
 #' @export
 #' @importFrom AirMonitor US_AQI
